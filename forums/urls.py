f9877dd54d8316d450cf1bf5d221434bf555a210
noxan <noxan@byteweaver.net>
2012-11-20 18:31:35 +0100

    add url for topic detail view

--- forums/urls.py
+++ forums/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from views import CategoryListView, ForumDetailView
+from views import CategoryListView, ForumDetailView, TopicDetailView
 
 
 urlpatterns = patterns('',
     url(r'^$', CategoryListView.as_view(), name='overview'),
     url(r'^(?P<pk>\d+)/$', ForumDetailView.as_view(), name='forum'),
+    url(r'^topic/(?P<pk>\d+)/$', TopicDetailView.as_view(), name='topic'),
 )
