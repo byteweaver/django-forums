e1e1e5a1bf122e24115b79358cb7f475e81360a0
noxan <noxan@byteweaver.net>
2012-11-20 16:27:44 +0100

    include forum detail view in urls

--- forums/urls.py
+++ forums/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from views import CategoryListView
+from views import CategoryListView, ForumDetailView
 
 
 urlpatterns = patterns('',
     url(r'^$', CategoryListView.as_view(), name='overview'),
+    url(r'^(?P<pk>\d+)/$', ForumDetailView.as_view(), name='forum'),
 )
