5a2e18cf3be7725c68ef7a144cc84065e79766c6
noxan <noxan@byteweaver.net>
2012-11-20 16:26:25 +0100

    implement forum detail view

--- forums/views.py
+++ forums/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from models import Category
+from models import Category, Forum
 
 
 class CategoryListView(ListView):
     model = Category
+
+class ForumDetailView(DetailView):
+    model = Forum
