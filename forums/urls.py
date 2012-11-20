f2b1269893dadbb9fe981695174c910bcded3b20
noxan <noxan@byteweaver.net>
2012-11-20 18:35:28 +0100

    add url for post create view

--- forums/urls.py
+++ forums/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from views import CategoryListView, ForumDetailView, TopicDetailView, TopicCreateView
+from views import CategoryListView, ForumDetailView, TopicDetailView, TopicCreateView, PostCreateView
 
 
 urlpatterns = patterns('',
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\d+)/$', ForumDetailView.as_view(), name='forum'),
     url(r'^(?P<forum_id>\d+)/create/$', TopicCreateView.as_view(), name='topic_create'),
     url(r'^topic/(?P<pk>\d+)/$', TopicDetailView.as_view(), name='topic'),
+    url(r'^topic/(?P<pk>\d+)/create/$', PostCreateView.as_view(), name='post_create'),
 )
