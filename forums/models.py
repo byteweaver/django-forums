bebd42de4657ba835f3f18669079f6ad150dcf1b
noxan <noxan@byteweaver.net>
2012-11-20 16:12:14 +0100

    add basic models for categories and forums

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(_("Name"), max_length=255, unique=True)
+
+
+class Forum(models.Model):
+    category = models.ForeignKey(Category, related_name='forums')
+    name = models.CharField(_("Name"), max_length=255)
+    description = models.TextField(_("Description"), blank=True)
