841f98e80ec7a6780c0763c433180e36bee19769
noxan <noxan@byteweaver.net>
2012-11-20 18:37:21 +0100

    add current forum to topic create view context

--- forums/views.py
+++ forums/views.py
@@ -17,6 +17,11 @@ class TopicCreateView(FormView):
     template_name = 'forums/topic_create.html'
     form_class = TopicCreateForm
 
+    def get_context_data(self, **kwargs):
+        context = super(TopicCreateView, self).get_context_data(**kwargs)
+        context['forum'] = Forum.objects.get(id=self.kwargs.get('forum_id', None))
+        return context
+
 
 class PostCreateView(FormView):
     template_name = 'forums/post_create.html'
