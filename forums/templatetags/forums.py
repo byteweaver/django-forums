8f8bad017b6f85ff197cc817693d5492c631e792
noxan <noxan@byteweaver.net>
2012-11-20 18:50:52 +0100

    create templattags to add popular topics to the current context

---
+++ forums/templatetags/forums.py
@@ -0,0 +1,17 @@
+from django import template
+
+from orbarena.forums.models import Topic
+
+
+register = template.Library()
+
+
+class PopularTopicsListNode(template.Node):
+    def render(self, context):
+        context['popular_topics_list'] = Topic.objects.all()[:5]
+        return u''
+
+
+@register.tag
+def get_popular_topics(parser, token):
+    return PopularTopicsListNode()
