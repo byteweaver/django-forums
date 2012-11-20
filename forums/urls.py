cd5294983aebe77316caa8e06bbd6ce088bd1e1d
noxan <noxan@byteweaver.net>
2012-11-20 18:39:16 +0100

    add missing login required decorators to post and topic create views

--- forums/urls.py
+++ forums/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from views import CategoryListView, ForumDetailView, TopicDetailView, TopicCreateView, PostCreateView
 
@@ -6,7 +7,7 @@ from views import CategoryListView, ForumDetailView, TopicDetailView, TopicCreat
 urlpatterns = patterns('',
     url(r'^$', CategoryListView.as_view(), name='overview'),
     url(r'^(?P<pk>\d+)/$', ForumDetailView.as_view(), name='forum'),
-    url(r'^(?P<forum_id>\d+)/create/$', TopicCreateView.as_view(), name='topic_create'),
+    url(r'^(?P<forum_id>\d+)/create/$', login_required(TopicCreateView.as_view()), name='topic_create'),
     url(r'^topic/(?P<pk>\d+)/$', TopicDetailView.as_view(), name='topic'),
-    url(r'^topic/(?P<pk>\d+)/create/$', PostCreateView.as_view(), name='post_create'),
+    url(r'^topic/(?P<pk>\d+)/create/$', login_required(PostCreateView.as_view()), name='post_create'),
 )
