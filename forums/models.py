530fe67fff921a3677c9ccedc5053a0c1e1ab357
noxan <noxan@byteweaver.net>
2012-11-20 16:12:56 +0100

    add basic models for topics and posts

--- forums/models.py
+++ forums/models.py
@@ -12,3 +12,18 @@ class Forum(models.Model):
     category = models.ForeignKey(Category, related_name='forums')
     name = models.CharField(_("Name"), max_length=255)
     description = models.TextField(_("Description"), blank=True)
+
+
+class Topic(models.Model):
+    forum = models.ForeignKey(Forum, related_name='topics')
+    name = models.CharField(_("Name"), max_length=255)
+    last_post = models.ForeignKey('Post', verbose_name=_("Last post"), related_name='forum_last_post', blank=True, null=True)
+
+
+
+class Post(models.Model):
+    topic = models.ForeignKey(Topic, related_name='posts')
+    user = models.ForeignKey(User, related_name='forum_posts')
+    created = models.DateTimeField(_("Created"), auto_now_add=True)
+    updated = models.DateTimeField(_("Updated"),auto_now=True)
+    body = models.TextField(_("Body"))
