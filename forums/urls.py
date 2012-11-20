c79cc31da834ffd8c4a3d4ceba7ca02802ab8e2d
noxan <noxan@byteweaver.net>
2012-11-20 18:31:48 +0100

    add url for topic create view

--- forums/urls.py
+++ forums/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url
 
-from views import CategoryListView, ForumDetailView, TopicDetailView
+from views import CategoryListView, ForumDetailView, TopicDetailView, TopicCreateView
 
 
 urlpatterns = patterns('',
     url(r'^$', CategoryListView.as_view(), name='overview'),
     url(r'^(?P<pk>\d+)/$', ForumDetailView.as_view(), name='forum'),
+    url(r'^(?P<forum_id>\d+)/create/$', TopicCreateView.as_view(), name='topic_create'),
     url(r'^topic/(?P<pk>\d+)/$', TopicDetailView.as_view(), name='topic'),
 )
