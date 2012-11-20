f471ef5d65184165891096619794f4b38a776ef2
noxan <noxan@byteweaver.net>
2012-11-20 18:33:06 +0100

    implement post create form

--- forums/forms.py
+++ forums/forms.py
@@ -4,3 +4,6 @@ from django import forms
 class TopicCreateForm(forms.Form):
     topic = forms.CharField("Topic", min_length=3, widget=forms.TextInput(attrs={'class': 'input-xxlarge'}))
     message = forms.CharField("Message", min_length=3, widget=forms.Textarea(attrs={'class': 'input-xxlarge'}))
+
+class PostCreateForm(forms.Form):
+    message = forms.CharField("Message", min_length=3, widget=forms.Textarea(attrs={'class': 'input-xxlarge'}))
