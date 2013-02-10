0bad9441811e59648bb2884f67a85119de591458
noxan <noxan@byteweaver.net>
2013-02-10 17:55:27 +0100

    improve display of forum admin (list_display)

--- forums/admin.py
+++ forums/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from models import Category, Forum, Topic, Post
 
 
+class ForumAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'position', 'is_closed')
+
+
 admin.site.register(Category)
-admin.site.register(Forum)
+admin.site.register(Forum, ForumAdmin)
 admin.site.register(Topic)
 admin.site.register(Post)
