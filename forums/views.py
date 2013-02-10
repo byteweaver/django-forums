a60ce084d8906ec9fcdf25f04f156380bed64188
noxan <noxan@byteweaver.net>
2013-02-10 18:15:58 +0100

    override dispatch functions in forum create views to check permissions first

--- forums/views.py
+++ forums/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView, FormView
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 from forms import TopicCreateForm, PostCreateForm
 from models import Category, Topic, Forum, Post
@@ -18,13 +21,19 @@ class TopicCreateView(FormView):
     template_name = 'forums/topic_create.html'
     form_class = TopicCreateForm
 
+    def dispatch(self, request, *args, **kwargs):
+        self.forum = Forum.objects.get(id=kwargs.get('forum_id', None))
+        if self.forum.is_closed and not request.user.is_staff:
+            messages.error(request, _("You do not have the permissions to create a topic."))
+            return HttpResponseRedirect(reverse_lazy('forums:forum', args=[self.forum.id]))
+        return super(TopicCreateView, self).dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
         topic_name = form.cleaned_data['topic']
         post_body = form.cleaned_data['message']
         user = self.request.user
-        forum = Forum.objects.get(id=self.kwargs.get('forum_id', None))
 
-        topic = Topic(forum=forum, name=topic_name)
+        topic = Topic(forum=self.forum, name=topic_name)
         topic.save()
         post = Post(topic=topic, body=post_body, user=user)
         post.save()
@@ -45,17 +54,23 @@ class PostCreateView(FormView):
     template_name = 'forums/post_create.html'
     form_class = PostCreateForm
 
+    def dispatch(self, request, *args, **kwargs):
+        self.topic = Topic.objects.get(id=kwargs.get('pk', None))
+        if self.topic.forum.is_closed and not request.user.is_staff:
+            messages.error(request, _("You do not have the permissions to create a topic."))
+            return HttpResponseRedirect(reverse_lazy('forums:forum', args=[self.topic.forum.id]))
+        return super(PostCreateView, self).dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
         body = form.cleaned_data['message']
         user = self.request.user
-        topic = Topic.objects.get(id=self.kwargs.get('pk', None))
 
-        post = Post(topic=topic, body=body, user=user)
+        post = Post(topic=self.topic, body=body, user=user)
         post.save()
         post.topic.last_post = post
         post.topic.save()
 
-        self.success_url = reverse('forums:topic', args=[topic.id])
+        self.success_url = reverse('forums:topic', args=[self.topic.id])
 
         return super(PostCreateView, self).form_valid(form)
 
