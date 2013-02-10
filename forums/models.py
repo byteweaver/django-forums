23eabd62680907148bff0dcb8123140ec42fbf85
noxan <noxan@byteweaver.net>
2013-02-10 17:54:11 +0100

    add is closed flag to forum model

--- forums/models.py
+++ forums/models.py
@@ -20,6 +20,7 @@ class Forum(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     position = models.IntegerField(_("Position"), default=0)
     description = models.TextField(_("Description"), blank=True)
+    is_closed = models.BooleanField(_("Is closed"), default=False)
 
     class Meta:
         ordering = ['position']
