141f904f4c3a1cd3d3876faf9d9315d6884f72a6
noxan <noxan@byteweaver.net>
2013-11-06 10:08:06 +0100

    remove custom attrs from forms

--- forums/forms.py
+++ forums/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class TopicCreateForm(forms.Form):
-    topic = forms.CharField("Topic", min_length=3, widget=forms.TextInput(attrs={'class': 'input-xxlarge'}))
-    message = forms.CharField("Message", min_length=3, widget=forms.Textarea(attrs={'class': 'input-xxlarge'}))
+    topic = forms.CharField("Topic", min_length=3)
+    message = forms.CharField("Message", min_length=3, widget=forms.Textarea())
 
 
 class PostCreateForm(forms.Form):
-    message = forms.CharField("Message", min_length=3, widget=forms.Textarea(attrs={'class': 'input-xxlarge'}))
+    message = forms.CharField("Message", min_length=3, widget=forms.Textarea())
