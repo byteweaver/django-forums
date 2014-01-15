5075c3542fb7724383fa98729d8957e2027da6f9
noxan <noxan@byteweaver.net>
2014-01-15 13:15:54 +0100

    add user factory for django user model

--- forums/tests/factories.py
+++ forums/tests/factories.py
@@ -1,7 +1,17 @@
 import factory
 from factory.django import DjangoModelFactory
 
+from django.contrib.auth import get_user_model
+
 from forums.models import Category, Forum, Topic
+
+
+class UserFactory(DjangoModelFactory):
+    FACTORY_FOR = get_user_model()
+
+    username = 'Username'
+    email = 'email@example.com'
+    password = '123456'
 
 
 class CategoryFactory(DjangoModelFactory):
