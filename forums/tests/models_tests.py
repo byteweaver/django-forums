d68d4b3dd66acdee0fe4af4dadcb74a962e6e696
noxan <noxan@byteweaver.net>
2014-01-15 12:58:05 +0100

    use category factory for category creation test

--- forums/tests/models_tests.py
+++ forums/tests/models_tests.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
 
 from forums.models import Category
+from forums.tests.factories import CategoryFactory
 
 
 class CategoryModelTest(TestCase):
     def test_category_creation(self):
-        category = Category.objects.create(**{
-            'name': 'Category',
-        })
+        category = CategoryFactory.create()
 
         self.assertEquals(category.name, 'Category')
