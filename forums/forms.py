17933131c3f78a41a4aa803959e31e200cecd97e
noxan <noxan@byteweaver.net>
2014-01-15 15:29:37 +0100

    add missing label declarator to all fields in forms

--- forums/forms.py
+++ forums/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class TopicCreateForm(forms.Form):
-    topic = forms.CharField("Topic", min_length=3)
-    message = forms.CharField("Message", min_length=3, widget=forms.Textarea())
+    topic = forms.CharField(label="Topic", min_length=3)
+    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea())
 
 
 class PostCreateForm(forms.Form):
-    message = forms.CharField("Message", min_length=3, widget=forms.Textarea())
+    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea())
