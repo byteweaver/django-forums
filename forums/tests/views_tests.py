31c173a915b6656315ae1ce9bd21a19ed4427fd5
noxan <noxan@byteweaver.net>
2014-01-15 15:41:19 +0100

    add plain get test cases for forum and topic views

--- forums/tests/views_tests.py
+++ forums/tests/views_tests.py
@@ -1,7 +1,27 @@
 from django.test import TestCase
 
+from forums.tests.factories import ForumFactory, TopicFactory
+
 
 class CategoryListViewTest(TestCase):
     def test_plain_get_request(self):
         response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+
+class ForumListViewTest(TestCase):
+    def setUp(self):
+        self.forum = ForumFactory.create()
+
+    def test_plain_get_request(self):
+        response = self.client.get('/%d/' % self.forum.pk)
+        self.assertEquals(response.status_code, 200)
+
+
+class TopicListViewTest(TestCase):
+    def setUp(self):
+        self.topic = TopicFactory.create()
+
+    def test_plain_get_request(self):
+        response = self.client.get('/topic/%d/' % self.topic.pk)
         self.assertEquals(response.status_code, 200)
