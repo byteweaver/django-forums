b95a605a0e48ac38db7e4cdbcce7bcfc0e6c8450
python-consulting <contact@python-consulting.com>
2014-01-30 17:37:32 +0100

    Reverts CSS classes to None

--- forums/forms.py
+++ forums/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class TopicCreateForm(forms.Form):
     topic = forms.CharField(label="Topic", min_length=3)
-    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea(attrs={'class':'form-control'}))
+    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea())
 
 
 class PostCreateForm(forms.Form):
-    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea(attrs={'class':'form-control'}))
+    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea())
