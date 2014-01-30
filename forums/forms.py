64c3847da111539252fa882b13b75187a7bf7d0d
python-consulting <contact@python-consulting.com>
2014-01-30 13:19:07 +0100

    Set bootstrap form-control class for both forms

--- forums/forms.py
+++ forums/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class TopicCreateForm(forms.Form):
     topic = forms.CharField(label="Topic", min_length=3)
-    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea())
+    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea(attrs={'class':'form-control'}))
 
 
 class PostCreateForm(forms.Form):
-    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea())
+    message = forms.CharField(label="Message", min_length=3, widget=forms.Textarea(attrs={'class':'form-control'}))
