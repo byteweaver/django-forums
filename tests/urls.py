813f4c797607f9ef5fc432bd244c1c14a9bd1768
Richard <richard@devisio.net>
2015-09-20 11:54:14 +0200

    add missing namespace to test url conf

--- tests/urls.py
+++ tests/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, patterns, url
 
 
 urlpatterns = patterns('',
-    url(r'^', include('forums.urls')),
+    url(r'^', include('forums.urls', namespace='forums')),
 )
